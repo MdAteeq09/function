dd4f895a2c7aad9cd7f6e852b78877eaad79dc71
Ateeq <mdateequrrahma123@gmail.com>
2025-08-15 22:21:22 +0530

    new file added

---
+++ fuction.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class fuction {
+    public static void main(String arug[]){
+
+        Scanner sc= new Scanner(System.in);
+         
+        System.out.println("Enter the number");
+        int digits = sc.nextInt();
+
+        System.out.println("The sum is" + sumDigits(digits));
+
+        
+    }
+    public static int sumDigits(int n){
+        int sumofDigits = 0;
+
+        while (n>0) {
+            int lastDigit = n % 10;
+            sumofDigits += lastDigit;
+            n /= 10;
+        }
+         return sumofDigits;
+         
+    }
+}
